51e67464d07e13c8af317e630579102d9024a0dd
EthanKIM <khg023246@gmail.com>
2023-02-21 14:05:48 +0900

    learn : output

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,35 @@
 // webpack.config.js
 // `webpack` command will pick up this config setup by default
 var path = require('path');
+// basic webpack config
 
-module.exports = {
+/* module.exports = {
   mode: 'none',
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
   }
-};
+}; */
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+  mode: 'none',
+  entry: './src/LoaderEx/app.js',
+  output: {
+    filename: 'css.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module:  {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ]
+}
